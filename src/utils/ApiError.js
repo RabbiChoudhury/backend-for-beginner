949544c1a6af014601976ea6f02287d0086a0e97
Rabbi Choudhury <frk.frb@gmail.com>
2026-02-09 23:51:39 +0600

    spelling mistake solve

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,20 +1,20 @@
 class ApiError extends Error{
     constructor(
-        statusCOde,
+        statusCode,
         message="Something went wrong",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message);
-        this.statusCode = statusCOde;
+        this.statusCode = statusCode;
         this.data = null;
         this.message = message;
         this.success = false;
         this.errors = errors;
         
 
-        if(statck){
-            this.stack = statck;
+        if(stack){
+            this.stack = stack;
         }
         else{
             Error.captureStackTrace(this,this.constructor);

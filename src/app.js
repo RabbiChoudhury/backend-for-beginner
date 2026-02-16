0f99e13f0954ed87970df175dcfc01610982e353
Rabbi Choudhury <frk.frb@gmail.com>
2026-02-16 18:39:49 +0600

    all setup configered

--- src/app.js
+++ src/app.js
@@ -20,6 +20,16 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use('/api/v1/users',userRouter)
+
+
+
 
 
 
